2367e12502151fdc23c0de499e7d19aebbde54b6
JuanDev <juanenriquegarcia126@gmail.com>
2024-04-06 16:08:22 -0400

    juego terminado

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import html from "./app.html?raw";
 import { perdiste } from "./components/mensaje-perdiste";
+import { palabraAleatoria } from "./components/palabra-aleatoria";
 
 let palabra = " ";
 let intentos = 0;
 
 export const App = (elementId) => {
-
   (() => {
     const app = document.createElement("div");
     app.innerHTML = html;
     document.querySelector(elementId).append(app);
   })();
-
 
   const opciones = document.querySelector(".option__position");
   const game__option = document.querySelector(".game__option");
@@ -28,10 +27,11 @@ export const App = (elementId) => {
   const lost__position = document.querySelector(".lost__position");
   const lost = document.querySelector(".lost");
   const imagenes = document.querySelectorAll("img");
-  const secret__text = document.querySelector('.secret__text')
-  const secret__winner = document.querySelector('.secret__winner')
-  const game__reload = document.querySelector('.game__reload')
-  const nuevoElementoLi = document.createElement('li');
+  const secret__text = document.querySelector(".secret__text");
+  const secret__winner = document.querySelector(".secret__winner");
+  const game__reload = document.querySelector(".game__reload");
+  const nuevoElementoLi = document.createElement("li");
+  const star__aleatorio = document.querySelector(".star__aleatorio");
 
   //block
   game__input.disabled = true;
@@ -39,28 +39,27 @@ export const App = (elementId) => {
   game__reload.disabled = true;
 
   //listeners
-  game__star.addEventListener('input',(event)=>{
+  game__star.addEventListener("input", (event) => {
     let inputValue = event.target.value;
-    let filteredValue = inputValue.replace(/[^A-Za-z]/g, ''); 
-    event.target.value = filteredValue; 
+    let filteredValue = inputValue.replace(/[^A-Za-z]/g, "");
+    event.target.value = filteredValue;
     event.target.value = filteredValue.toUpperCase();
-    game__abc.innerHTML = '';
+    game__abc.innerHTML = "";
     const palabras = game__star.value.split(/\s+/);
-    palabras.forEach(function(palabra) {
-        if (palabra !== '') { 
-            nuevoElementoLi.textContent = '_'.repeat(palabra.length); 
-            game__abc.appendChild(nuevoElementoLi);
-        }
+    palabras.forEach(function (palabra) {
+      if (palabra !== "") {
+        nuevoElementoLi.textContent = "_".repeat(palabra.length);
+        game__abc.appendChild(nuevoElementoLi);
+      }
     });
-  })
+  });
 
-
-  game__input.addEventListener('input',(event)=>{
+  game__input.addEventListener("input", (event) => {
     let inputValue = event.target.value;
-    let filteredValue = inputValue.replace(/[^A-Za-z]/g, ''); 
-    event.target.value = filteredValue; 
+    let filteredValue = inputValue.replace(/[^A-Za-z]/g, "");
+    event.target.value = filteredValue;
     event.target.value = filteredValue.toUpperCase();
-  })
+  });
 
   game__option.addEventListener("click", () => {
     opciones.style.display = "inline";
@@ -80,12 +79,11 @@ export const App = (elementId) => {
       game__reload.disabled = false;
       intentos = 0;
       incrementar.textContent = 0;
-  
-      
-      game__abc.innerHTML = '';
+
+      game__abc.innerHTML = "";
       for (let i = 0; i < palabra.length; i++) {
-        const nuevoElementoLi = document.createElement('li');
-        nuevoElementoLi.textContent = '_';
+        const nuevoElementoLi = document.createElement("li");
+        nuevoElementoLi.textContent = "_";
         game__abc.appendChild(nuevoElementoLi);
       }
     }
@@ -93,8 +91,8 @@ export const App = (elementId) => {
 
   game__send.addEventListener("click", () => {
     const letraIngresada = game__input.value.toUpperCase();
-    let aciertos = 0; 
-  
+    let aciertos = 0;
+
     if (game__input.value.length === palabra.length) {
       if (game__input.value === palabra) {
         winer__position.style.display = "inline";
@@ -102,30 +100,37 @@ export const App = (elementId) => {
         game__input.disabled = true;
         secret__winner.textContent = `"${palabra}"`;
         game__reload.disabled = true;
-        return; 
+        return;
       }
     }
-  
+
     for (let i = 0; i < palabra.length; i++) {
       if (palabra[i] === letraIngresada) {
         game__abc.children[i].textContent = letraIngresada;
         aciertos++;
       }
     }
-  
+
     if (aciertos === 0) {
       intentos++;
       incrementar.textContent = intentos;
       imagenes[intentos].style.display = "inline";
-  
+
       if (intentos === 7) {
         game__send.disabled = true;
         game__input.disabled = true;
         game__reload.disabled = true;
         secret__text.textContent = `"${palabra}"`;
         setTimeout(() => {
-          game__abc.innerHTML = '';
-          perdiste(intentos, lost__position, game__star, game__send, game__input, game__reload);
+          game__abc.innerHTML = "";
+          perdiste(
+            intentos,
+            lost__position,
+            game__star,
+            game__send,
+            game__input,
+            game__reload
+          );
         }, 200);
       }
     } else {
@@ -139,18 +144,37 @@ export const App = (elementId) => {
     }
   });
 
-  game__reload.addEventListener('click',()=>{
-      star__position.style.display = "inline";
-      game__send.disabled = true;
-      game__input.disabled = true;
-      game__reload.disabled = true;
-      game__star.value = "";
-      game__input.value = "";
-      intentos = 0;
-      incrementar.textContent = 0;
-      game__abc.innerHTML = '';
-      imagenes.forEach((img) => (img.style.display = "none"));
-  })
+  game__reload.addEventListener("click", () => {
+    star__position.style.display = "inline";
+    game__send.disabled = true;
+    game__input.disabled = true;
+    game__reload.disabled = true;
+    game__star.value = "";
+    game__input.value = "";
+    intentos = 0;
+    incrementar.textContent = 0;
+    game__abc.innerHTML = "";
+    imagenes.forEach((img) => (img.style.display = "none"));
+  });
+
+  star__aleatorio.addEventListener("click", () => {
+    const indiceAleatorio = Math.floor(Math.random() * palabraAleatoria.length);
+    palabra = palabraAleatoria[indiceAleatorio].toUpperCase();
+    star__position.style.display = "none";
+    game__send.disabled = false;
+    game__input.disabled = false;
+    game__reload.disabled = false;
+    intentos = 0;
+    incrementar.textContent = 0;
+
+    game__abc.innerHTML = "";
+
+    for (let i = 0; i < palabra.length; i++) {
+      const nuevoElementoLi = document.createElement("li");
+      nuevoElementoLi.textContent = "_";
+      game__abc.appendChild(nuevoElementoLi);
+    }
+  });
 
   winner.addEventListener("click", () => {
     if ((winer__position.style.display = "none")) {
@@ -161,7 +185,7 @@ export const App = (elementId) => {
       game__input.value = "";
       star__position.style.display = "inline";
       imagenes.forEach((img) => (img.style.display = "none"));
-      game__abc.innerHTML = '';
+      game__abc.innerHTML = "";
     }
   });
 
@@ -174,9 +198,7 @@ export const App = (elementId) => {
       game__input.value = "";
       star__position.style.display = "inline";
       imagenes.forEach((img) => (img.style.display = "none"));
-      game__abc.innerHTML = '';
+      game__abc.innerHTML = "";
     }
   });
-
-
 };
